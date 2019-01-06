bc62c38f1745baa64db180938882b26a190f9dcd
Lucas Diogo França <lucasdiogo1234@gmail.com>
2019-01-06 09:59:34 -0200

    Update Pizza.java
    Alterações no texto.

--- src/ImplementacaoPobre/Pizza.java
+++ src/ImplementacaoPobre/Pizza.java
@@ -4,15 +4,15 @@ abstract class Pizza {
 	public abstract void preparar();
 
 	public void assar() {
-		System.out.println("assar a 300 graus...");
+		System.out.println("assar a 300 graus!");
 	}
 
 	public void cortar() {
-		System.out.println("cortando na diagonal...");
+		System.out.println("cortando na diagonal!");
 	}
 
 	public void embalar() {
-		System.out.println("embalando para viagem...");
+		System.out.println("embalando para viagem!");
 	}
 }
 
