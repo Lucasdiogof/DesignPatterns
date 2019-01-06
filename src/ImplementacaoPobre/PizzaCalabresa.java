03dc3b5c1e6e72d80cb8be95629c13880524eae6
Lucas Diogo França <lucasdiogo1234@gmail.com>
2019-01-06 09:59:54 -0200

    Update PizzaCalabresa.java
    Alterações no texto.

--- src/ImplementacaoPobre/PizzaCalabresa.java
+++ src/ImplementacaoPobre/PizzaCalabresa.java
@@ -2,6 +2,6 @@ package ImplementacaoPobre;
 
 class PizzaCalabresa extends Pizza {
 	public void preparar() {
-		System.out.println("Preparando uma pizza calabresa...");
+		System.out.println("Preparando uma pizza calabresa!");
 	}
 }
